8ff1216d45fc0b1486f1faf23b2d43867281295b
lovethree4 <138865826+lovethree4@users.noreply.github.com>
2023-07-30 18:03:00 +0900

    Add files via upload

---
+++ Ex1_hello.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class Ex1_hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		System.out.println("Jave");
+		System.out.print("World");
+		
+				
+	}
+
+}
