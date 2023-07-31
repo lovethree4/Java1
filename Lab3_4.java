db29ff143e080db6c3799f1122f857be00d1c219
lovethree4 <138865826+lovethree4@users.noreply.github.com>
2023-07-31 16:05:02 +0900

    Add files via upload

--- Lab3_4.java
+++ Lab3_4.java
@@ -9,15 +9,15 @@ public class Lab3_4 {
 		int num1 = scanner.nextInt();
 		double num2 = scanner.nextDouble();
 		
-		double result =num2/3;
-		long result  = Math.round ();
+		double result =num2 /3;
+		System.out.println("num2 = " + num1);
 		System.out.println(result);
 		
 		double result2 = num2 / 4;
-		long result2 = Math.round ();
+		System.out.println("num2 = " + num1);
 		System.out.println(result2);
 		
-		System.out.println("치킨은"+result +"마리,"+"피자는 result2+"판 주문해야 합니다.");
+		System.out.println("치킨은"+result +"마리,"+"피자는"+result2+"판 주문해야 합니다.");
 		
 		
 		// TODO Auto-generated method stub
